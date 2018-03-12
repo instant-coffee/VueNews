8e4e143dff4efeedb84cd7cdfe03b17481ecffc6
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-03-12 10:05:35 -0700

    Initial commit -- Working

---
+++ pages/New.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1> NEW
+    </h1>
+    <Items></Items>
+  </div>
+</template>
+
+<script>
+import Items from "~/components/Items.vue";
+export default {
+  components:{
+    Items
+  },
+  async fetch({ store }) {
+    await store.dispatch("LOAD_ITEMS", "newstories.json")
+  }
+}
+</script>
+
+
